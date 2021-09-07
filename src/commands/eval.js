609cb01840143bc4483b356cb5c93180a3a7482a
itzJOHv <me@djoh.ml>
2021-09-07 20:36:19 +0300

    Update eval.js

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -13,7 +13,7 @@ module.exports.run = async (message, args) => {
 		let evaled = await eval(content);
 		if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
 
-		if (String(evaled).length >= 2000) return message.react("✅");
+		if (String(evaled).length >= 4000) return message.react("✅");
 
 		message.channel.send(evaled, { code: "js", split: true });
 	} catch (e) {
