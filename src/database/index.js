eb32f8a859b84ac00993a8134b56b9df2707cfd6
itzJOHv <me@djoh.xyz>
2021-10-10 16:39:42 +0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,19 +1,12 @@
 const config = require("../../config"), mongoose = require("mongoose");
 
-module.exports = async () => {
-    await mongoose.connect(config.database_uri, {
+module.exports.guild = require("./guild")();
+module.exports.cacheGuilds = require("./guild").cacheAll;
+module.exports.global = require("./global");
+
+module.exports.connection = mongoose.connect(config.database_uri, {
         useNewUrlParser: true,
         useFindAndModify: false,
         useCreateIndex: true,
         useUnifiedTopology: true
-    }).catch(e => {
-        log.error(e.message + ": " + e.stack);
-        client.shard.send("respawn");
-    });
-
-    return {
-        guild: require("./guild")(), // guild(guildid)
-        cacheGuilds: require("./guild").cacheAll,
-        global: require("./global.js")
-    };
-};
+});
