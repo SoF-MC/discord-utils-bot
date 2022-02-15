d43eb333030f72331a55c7e844a729f28476449b
itzJOHv <me@djoh.xyz>
2022-02-15 21:34:09 +0200

    Update sharding.js

--- src/sharding.js
+++ src/sharding.js
@@ -1,10 +1,8 @@
 require("nodejs-better-console").overrideConsole();
 const Discord = require("discord.js");
-const config = require("../config");
 
 const manager = new Discord.ShardingManager("./src/bot.js", {
     totalShards: 1,
-    token: config.token,
     mode: "worker"
 });
 
