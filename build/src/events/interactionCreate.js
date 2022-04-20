9ff3b00f4ecadfc0315780f0f0ea9156944103fa
itzJOHv <me@djoh.xyz>
2022-04-20 10:00:07 +0300

    .

--- build/src/events/interactionCreate.js
+++ build/src/events/interactionCreate.js
@@ -1,6 +1,9 @@
 "use strict";
 const buttons_1 = require("../handlers/buttons");
+const commands_1 = require("../handlers/commands");
 module.exports = async (interaction) => {
+    if (interaction.isCommand())
+        await (0, commands_1.processCommand)(interaction);
     if (interaction.isButton())
         await (0, buttons_1.processButton)(interaction);
 };
