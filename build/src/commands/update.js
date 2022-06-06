1467a930f2406be410b249fc5a3cf68b876b7d20
Alex <me@djoh.xyz>
2022-06-06 20:37:42 +0300

    updates

--- build/src/commands/update.js
+++ build/src/commands/update.js
@@ -13,7 +13,7 @@ module.exports = {
             if (err)
                 return console.error(err);
             const str = stdout || stderr;
-            if (str.includes("Already up-to-date."))
+            if (str.includes("Already up to date."))
                 return await interaction.editReply("Already up-to-date.");
             await interaction.editReply("```\n" + (stdout || stderr).substring(0, 1992) + "\n```");
             process.exit(0);
--- src/commands/update.ts
+++ src/commands/update.ts
@@ -14,7 +14,7 @@ export = {
             if (err) return console.error(err);
             const str = stdout || stderr;
 
-            if (str.includes("Already up-to-date.")) return await interaction.editReply("Already up-to-date.");
+            if (str.includes("Already up to date.")) return await interaction.editReply("Already up-to-date.");
 
             await interaction.editReply("```\n" + (stdout || stderr).substring(0, 1992) + "\n```");
 
