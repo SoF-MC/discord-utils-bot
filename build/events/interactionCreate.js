356078995e48a111cc831edd6c3c89bfadb4193e
Vadym <me@djoh.xyz>
2022-10-23 15:53:24 +0000

    remove build folder

--- build/events/interactionCreate.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const buttons_1 = require("../handlers/buttons");
-const commands_1 = require("../handlers/commands");
-module.exports = async (interaction) => {
-    if (interaction.isCommand())
-        await (0, commands_1.processCommand)(interaction);
-    if (interaction.isButton())
-        await (0, buttons_1.processButton)(interaction);
-};
