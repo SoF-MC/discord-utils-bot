4f1415e0cb10dc4a8cabdd25f2cf11d866b9b5da
PadowYT2 <me@padow.ru>
2023-10-04 21:36:59 +0300

    патом

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,10 +1,10 @@
 import type { Message } from "discord.js";
 import mentionCommands from "../handlers/mentionCommands";
 import config from "../config";
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 
-function escapeString(word: string) {
+export function escapeString(word: string) {
     return word.replace(/"/g, '\\"');
 }
 
@@ -14,53 +14,48 @@ export async function run(message: Message<true>) {
     if (message.content.startsWith(`<@${message.client.user.id}> `) && config.admins.includes(message.author.id))
         return mentionCommands(message);
 
-    let counter = 0;
+    // let counter = 0;
+    // const filename = path.join("databases", `wordsdb_${message.guildId}.txt`);
 
-    console.log(path.join("databases", `wordsdb_${message.guildId}.txt`));
-    console.log(fs.existsSync(path.join("databases", `wordsdb_${message.guildId}.txt`)));
+    // if (!fs.existsSync(filename)) fs.writeFileSync(filename, "");
 
-    const filename = path.join("databases", `wordsdb_${message.guildId}.txt`);
+    // if (message.author.id !== "824379835262304257") {
+    //     const words = message.content.split(" ");
+    //     fs.appendFileSync(filename, words.map(escapeString).join(" ") + " ");
+    // }
 
-    if (!fs.existsSync("databases")) fs.mkdirSync("databases");
-    if (!fs.existsSync(filename)) fs.writeFileSync(filename, "");
+    // const handleCounter = async () => {
+    //     counter++;
+    //     if (counter >= 25) {
+    //         const wordsStr = fs.readFileSync(filename, "utf8");
+    //         const words = wordsStr.split(" ");
+    //         if (words.length > 0) {
+    //             const numWords = Math.min(Math.floor(Math.random() * 10) + 1, words.length);
+    //             const expression = words.slice(0, numWords).join(" ");
+    //             await message.channel.send(expression);
+    //         }
 
-    if (message.author.id !== "824379835262304257") {
-        const words = message.content.split(" ");
-        fs.appendFileSync(filename, words.map(escapeString).join(" ") + " ");
-    }
+    //         counter = 0;
+    //     }
+    // };
 
-    const handleCounter = async () => {
-        counter++;
-        if (counter >= 25) {
-            const wordsStr = fs.readFileSync(filename, "utf8");
-            const words = wordsStr.split(" ");
-            if (words.length > 0) {
-                const numWords = Math.min(Math.floor(Math.random() * 10) + 1, words.length);
-                const expression = words.slice(0, numWords).join(" ");
-                await message.channel.send(expression);
-            }
+    // if (message.guildId === "764178286233518100") {
+    //     if (message.channel.id === "764179432147124304") {
+    //         const wordsStr = fs.readFileSync(filename, "utf8");
+    //         const words = wordsStr.split(" ");
+    //         if (words.length > 0) {
+    //             const numWords = Math.min(Math.floor(Math.random() * 15) + 1, words.length);
+    //             const expression = words.slice(0, numWords).join(" ");
+    //             await message.channel.send(expression);
+    //         }
 
-            counter = 0;
-        }
-    };
+    //         counter = 0;
+    //     }
 
-    if (message.guildId === "764178286233518100") {
-        if (message.channel.id === "764179432147124304") {
-            const wordsStr = fs.readFileSync(filename, "utf8");
-            const words = wordsStr.split(" ");
-            if (words.length > 0) {
-                const numWords = Math.min(Math.floor(Math.random() * 15) + 1, words.length);
-                const expression = words.slice(0, numWords).join(" ");
-                await message.channel.send(expression);
-            }
-
-            counter = 0;
-        }
-
-        if (counter > 0) handleCounter();
-        if (message.mentions.has("824379835262304257")) handleCounter();
-    } else {
-        if (counter > 0) handleCounter();
-        if (message.mentions.has("824379835262304257")) handleCounter();
-    }
+    //     if (counter > 0) handleCounter();
+    //     if (message.mentions.has("824379835262304257")) handleCounter();
+    // } else {
+    //     if (counter > 0) handleCounter();
+    //     if (message.mentions.has("824379835262304257")) handleCounter();
+    // }
 }
